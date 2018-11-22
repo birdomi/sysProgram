bc3d3ab3e3838caa4b13fe7b837d5ccfea00f807
birdomi <ryk012@naver.com>
2018-11-22 19:44:38 +0900

    Update curses.c

--- curses.c
+++ curses.c
@@ -13,7 +13,7 @@ int main()
     printf("Press E to Exit\n");
 
     while((ch=getch())!='e'){
-        printf("%d pressed\n",ch);
+        printw("%d pressed\r",ch);
     }   
 
     printf("\nExiting Now\n");
