e4130f6a364ca397c0cdb63eeef44e76b191a756
birdomi <ryk012@naver.com>
2018-11-22 20:09:49 +0900

    Update curses.c

--- curses.c
+++ curses.c
@@ -13,7 +13,15 @@ int main()
     printf("Press E to Exit\n");
 
     while((ch=getch())!='e'){
-        printw("%d pressed\r",ch);
+        switch(ch){
+            case KEY_UP: printw("\rUp Arrow");
+                         break;
+            case KEY_DOWN: printw("\rDown Arrow");
+                         break;
+            default:
+                printw("\rThe pressed key is %c",ch);
+        }
+        
     }   
 
     printf("\nExiting Now\n");
