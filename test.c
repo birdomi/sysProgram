b79429a6e6f2d936b7ccade04484742e199bd4ff
birdomi <ryk012@naver.com>
2018-11-22 16:39:48 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int fd, n;
+    off_t start, cur;
+    char buf[10];
+
+    fd = open("~/.vimrc", O_RDONLY);
+    if(fd==-1){
+        perror("open .vimrc.");
+        exit(1);
+    }
+
+    while((n=read(fd,buf,10))>0){
+        if(n==-1){
+            perror("Read Error");
+            exit(1);
+        }
+        printf("%s\n",buf);
+    }
+}
