b19d1239c93bc8b1aff86809f3cd8ab660844842
birdomi <ryk012@naver.com>
2018-11-23 12:38:20 +0900

    Update newsh.c

--- newsh.c
+++ newsh.c
@@ -1,78 +1,73 @@
 #include "newsh.h"
 #include <stdlib.h>
 #include <fcntl.h>
-#include <ncurses/curses.h>
 #include <stdio.h>
 
-static char inpbuf[MAXBUF], tokbuf[2*MAXBUF], *ptr, *tok;
-static char special[] = {' ', '\t', '&', ';', '\n', '\0'};
+static char inpbuf[MAXBUF], tokbuf[2 * MAXBUF], *ptr, *tok;
+static char special[] = { ' ', '\t', '&', ';', '\n', '\0' };
 
 char *prompt = "Command>";
 
-int userin(char *p){
-    int c, count;
+int userin(char *p) {
+	int c, count;
 
-    ptr = inpbuf;
-    tok = tokbuf;
+	ptr = inpbuf;
+	tok = tokbuf;
 
-    
-    count = 0;       
-    
-    printw("%s ", p);
-    while(1){
-        c=getch();
-        if(c!=KEY_UP && c!=KEY_DOWN){
-            c=(char)c;
-            printw("%c",c);
-        }
-        if(c==EOF) return EOF;
-        if(count <MAXBUF) inpbuf[count++] = c;
-        if(c=='\n' && count <MAXBUF){
-            inpbuf[count] = '\0';            
-            return count;
-        }        
-        if(c=='\n'){
-            printw(" smallsh : input line too long\n");
-            count = 0;
-            printw("%s ", p);
-        }
-    }    
-    
+
+	count = 0;
+
+	printw("%s ", p);
+	while (1) {
+		c = getchar();
+		if (c == EOF) return EOF;
+		if (count <MAXBUF) inpbuf[count++] = c;
+		if (c == '\n' && count <MAXBUF) {
+			inpbuf[count] = '\0';
+			return count;
+		}
+		if (c == '\n') {
+			printf(" smallsh : input line too long\n");
+			count = 0;
+			printf("%s ", p);
+		}
+	}
+
 }
 
-int inarg(char c){
-    char *wrk;
-    for(wrk = special; *wrk != '\0'; wrk++) {
-        if (c == *wrk) {
-            return(0);
-        }
-    }
-    return(1);
+int inarg(char c) {
+	char *wrk;
+	for (wrk = special; *wrk != '\0'; wrk++) {
+		if (c == *wrk) {
+			return(0);
+		}
+	}
+	return(1);
 }
 
 //get token and place into tokbuf
-int get_token(char **outptr){
-    int type;
+int get_token(char **outptr) {
+	int type;
 
-    *outptr = tok;
-    while(*ptr == ' '||*ptr == '\t')
-        ptr++;
-        
-    *tok++= *ptr;
+	*outptr = tok;
+	while (*ptr == ' ' || *ptr == '\t')
+		ptr++;
 
-    switch(*ptr++) {
-        case '\n' : type = EOL;
-            break;
-        case '&' : type = AMPERSAND;
-            break;
-        case ';' : type = SEMICOLON;
-            break;
-        default : type = ARG;
-            while(inarg(*ptr))
-                *tok++ = *ptr++;
-    }
-    *tok++ = '\0';
-    return type;
+	*tok++ = *ptr;
+
+	switch (*ptr++) {
+	case '\n': type = EOL;
+		break;
+	case '&': type = AMPERSAND;
+		break;
+	case ';': type = SEMICOLON;
+		break;
+	default: type = ARG;
+		while (inarg(*ptr))
+			*tok++ = *ptr++;
+	}
+	*tok++ = '\0';
+	return type;
 }
 /* 입력 줄을 아래와 같이 처리한다 : */
 /* */
@@ -82,29 +77,29 @@ int get_token(char **outptr){
 /* runcommand라 불리는 루틴을 호출한다. */
 void procline()
 {
-    char *arg[MAXARG+1]; /* runcommand를 위한 포인터 배열 */
-    int toktype; /* 명령내의 토근의 유형 */
-    int narg; /* 지금까지의 인수 수 */
-    int type; /* FOREGROUND or BACKGROUND */
-    /* 토큰 유형에 따라 행동을 취한다. */
-    for (narg = 0;;) { /* loop FOREVER */
-        switch(toktype = get_token(&arg[narg])) {
-            case ARG : 
-                if (narg < MAXARG) narg++;
-                break;
-            case EOL :
-            case SEMICOLON :
-            case AMPERSAND :
-                type = (toktype == AMPERSAND) ? BACKGROUND : FOREGROUND;                
-                if (narg != 0) {
-                    arg[narg] = NULL;
-                    runcommand(arg, type);
-                }
-                if (toktype == EOL) return;
-                narg = 0;
-                break;
-        }
-    }
+	char *arg[MAXARG + 1]; /* runcommand를 위한 포인터 배열 */
+	int toktype; /* 명령내의 토근의 유형 */
+	int narg; /* 지금까지의 인수 수 */
+	int type; /* FOREGROUND or BACKGROUND */
+			  /* 토큰 유형에 따라 행동을 취한다. */
+	for (narg = 0;;) { /* loop FOREVER */
+		switch (toktype = get_token(&arg[narg])) {
+		case ARG:
+			if (narg < MAXARG) narg++;
+			break;
+		case EOL:
+		case SEMICOLON:
+		case AMPERSAND:
+			type = (toktype == AMPERSAND) ? BACKGROUND : FOREGROUND;
+			if (narg != 0) {
+				arg[narg] = NULL;
+				runcommand(arg, type);
+			}
+			if (toktype == EOL) return;
+			narg = 0;
+			break;
+		}
+	}
 }
 /* wait를 선택사항으로 하여 명령을 수행한다. */
 /* 명령을 수행하기 위한 모든 프로세스를 시작하게 한다. */
@@ -113,49 +108,44 @@ void procline()
 /* 단순히 프로세스 식별번호만 인쇄하고 복귀한다. */
 int runcommand(char **cline, int where)
 {
-    int status;
-    int pid, exitstat, ret;
+	int status;
+	int pid, exitstat, ret;
 
-    //history를 위해 입력한 명령 저장.
-    int fd, n;
-    fd = open(".history",O_RDWR|O_CREAT|O_APPEND,0644);
-    if(fd==-1){
-        perror("open .history");
-        exit(1);
-    }    
-    write(fd,inpbuf,MAXBUF);
-    close(fd);
-    //***
-    
-    if ((pid = fork()) < 0) {
-        perror("smallsh");
-        return(-1);
-    }
-    if (pid == 0) { /* child */        
-        execvp(*cline, cline);
-        perror(*cline);
-        exit(127);
-    }
-    /* code for parent */
-    /* if background process, print pid and exit */
-    if (where == BACKGROUND) {
-        printw("[Process id %d]\n",pid);
-        return(0);
-    }
-    /* 프로세스 pid가 퇴장할 때까지 기다린다. */
-    if (waitpid(pid, &status, 0) == -1)
-        return (-1);
-    else
-        return (status);
+	//history를 위해 입력한 명령 저장.
+	int fd, n;
+	fd = open(".history", O_RDWR | O_CREAT | O_APPEND, 0644);
+	if (fd == -1) {
+		perror("open .history");
+		exit(1);
+	}
+	write(fd, inpbuf, MAXBUF);
+	close(fd);
+	//***
+
+	if ((pid = fork()) < 0) {
+		perror("smallsh");
+		return(-1);
+	}
+	if (pid == 0) { /* child */
+		execvp(*cline, cline);
+		perror(*cline);
+		exit(127);
+	}
+	/* code for parent */
+	/* if background process, print pid and exit */
+	if (where == BACKGROUND) {
+		printf("[Process id %d]\n", pid);
+		return(0);
+	}
+	/* 프로세스 pid가 퇴장할 때까지 기다린다. */
+	if (waitpid(pid, &status, 0) == -1)
+		return (-1);
+	else
+		return (status);
 }
 int main()
-{    
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    noecho();
-    while(userin(prompt) != EOF){
-        procline();            
-    }
-    endwin();
+{
+	while (userin(prompt) != EOF) {
+		procline();
+	}
 }
