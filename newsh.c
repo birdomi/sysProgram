329d78dd36cf4d105360080195125a13dfb6e268
birdomi <ryk012@naver.com>
2018-11-23 12:58:05 +0900

    Update newsh.c

--- newsh.c
+++ newsh.c
@@ -20,6 +20,7 @@ int userin(char *p) {
 	printf("%s ", p);
 	while (1) {
 		c = getchar();
+		printf("%d", c);
 		if (c == EOF) return EOF;
 		if (count <MAXBUF) inpbuf[count++] = c;
 		if (c == '\n' && count <MAXBUF) {
