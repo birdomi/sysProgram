168c28d964308403881942e50bfe503dad19ee77
birdomi <ryk012@naver.com>
2018-11-25 17:12:24 +0900

    Update history.c

--- history.c
+++ history.c
@@ -15,7 +15,7 @@ int main(){
         exit(1);
     }
     while((n=read(fd,*buf,MAXBUF))>0){
-        printf("%d %s",lineNumber++,*buf++);
+        printf("%d %s",lineNumber++,buf++);
     }
 
     return 0;
