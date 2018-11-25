a2bf35bfa7c294cbf63b9a7c3b034389566c82f5
birdomi <ryk012@naver.com>
2018-11-25 17:18:18 +0900

    Update history.c

--- history.c
+++ history.c
@@ -6,16 +6,33 @@
 
 int main(){
     int fd,n;
-    char* buf[MAXBUF];
+    char buf[MAXBUF];
 
     int lineNumber=0;
+    int lineSelection=0;
+
     fd = open(".history",O_RDONLY);
     if(fd == -1){
         perror("read .history");
         exit(1);
     }
-    while((n=read(fd,*buf,MAXBUF))>0){
-        printf("%d %s",lineNumber++,buf++);
+    while((n=read(fd,buf,MAXBUF))>0){
+        lineNumber++;
+    }
+    
+    if(lineNumber>20){
+        lseek(fd,-20,SEEK_END);
+        while((n=read(fd,buf,MAXBUF))>0){
+            printf("%d %s",lineSelection,buf);
+            lineSelection++;
+        }
+    }
+    else{
+        lseek(fd,0,SEEK_SET);
+        while((n=read(fd,buf,MAXBUF))>0){
+            printf("%d %s",lineSelection,buf);
+            lineSelection++;
+        }
     }
 
     return 0;
