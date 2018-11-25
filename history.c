c8da82c972185f2d43dc52269ee7327e7eb624b0
birdomi <ryk012@naver.com>
2018-11-25 16:54:10 +0900

    Update history.c

--- history.c
+++ history.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+    int fd,n;
+    char buf[10];
+
+    int lineNumber=0;
+    fd = open(".history",O_RDONLY);
+    if(fd == -1){
+        perror("read .history");
+        exit(1);
+    }
+    while((n=read(fd,buf,10))>0){
+        printf("%d %s",Linenumber++,buf);
+    }
+    return 0;
+}
