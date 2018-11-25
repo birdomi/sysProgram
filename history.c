8758857f5465cbb9cd6fe8d7bca75118616e375f
birdomi <ryk012@naver.com>
2018-11-25 17:33:05 +0900

    Update history.c

--- history.c
+++ history.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "newsh.h"
+#include <ctype.h>
 
 int main(){
     int fd,n;
     char buf[MAXBUF];
+    char 
 
     int lineNumber=0;
     int lineSelection=0;
+    int s;
 
     fd = open(".history",O_RDONLY);
     if(fd == -1){
@@ -34,6 +37,24 @@ int main(){
             lineSelection++;
         }
     }
+
+    while(1){
+        s=scanf("\nselect line to reuse:%d%d");
+        if(s<lineSelection&&s>=0)
+            break;  
+        else{
+            printf("잘못된 입력");
+            exit(1);
+        }
+    }
+    if(lineNumber>20){
+        lseek(fd,20-s,SEEK_END);
+    }
+    else{
+        lseek(fd,s,SEEK_SET);
+    }
+    read(fd,buf,MAXBUF);
+    printf("result: %s \n",buf);
 
     return 0;
 }
