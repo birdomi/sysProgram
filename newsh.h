152bd0f9b6c427d86b95166b10d1dc980d454e67
birdomi <ryk012@naver.com>
2018-11-25 19:58:14 +0900

    Update newsh.h

--- newsh.h
+++ newsh.h
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "newsh.h"
+#include <ctype.h>
+
 #define EOL 1 /* End Of Line */
 #define ARG 2 /* normal argument */
 #define AMPERSAND 3
