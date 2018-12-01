3cf5957c71c0780d893b3bb46ad7b0fb1aef246b
birdomi <ryk012@naver.com>
2018-12-01 12:51:18 +0900

    Create alias.c

---
+++ alias.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argc){
+    int fd;
+    fd = open(".alias",O_RDWR|O_APPEND|O_CREAT);
+    if(fd == -1){
+        perror("open .alias");
+        exit(1);
+    }
+
+    if(argc==1){
+        printf("use -a for add alias\n");
+        printf("use -d for delete alias\n");
+    }
+    else if(argc ==3){
+
+    }
+    else{
+        printf("error\n");
+        exit(1);
+    }
+}
