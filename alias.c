bd6e42bdead1b7dd7578f06c4bca700efcdb4d23
birdomi <ryk012@naver.com>
2018-12-01 13:04:35 +0900

    Update alias.c

--- alias.c
+++ alias.c
@@ -5,16 +5,38 @@
 
 int main(int argc, char* argv[]){
     int fd;
+    int option;
+    int n;
     
     if(argc==1){
         printf("use -a for add alias\n");
         printf("use -d for delete alias\n");
     }
     else if(argc ==3){
+        if(argv[2]=="-a")
+            option=1;
+        else if(argv[2]=="-d")
+            option=0;
+        else{
+            printf("Unknown option\n");
+            exit(1);
+        }        
         fd = open(".alias",O_RDWR|O_APPEND|O_CREAT,0644);
         if(fd == -1){
             perror("open .alias");
             exit(1);
+        }
+        if(option){
+            n=write(".alias",argv[3],arg[3].length);
+            if(n<0){
+                perror("write .alias");
+                exit(1);
+            }
+            close(fd);
+            return 0;
+        }
+        else{
+            
         }
     }
     else{
