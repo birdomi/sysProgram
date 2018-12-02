fff6eae1d0ff770eb5586c9cec9c90141505d51c
birdomi <ryk012@naver.com>
2018-12-02 17:33:54 +0900

    Update alias.c

--- alias.c
+++ alias.c
@@ -6,21 +6,43 @@
 
 #define MAX 256
 
+int checkName(int fd,char* name){
+    
+}
+
 int main(int argc, char* argv[]){
     int fd;
     int option;
     int n;
-    char inpbuf[MAX];
+    char buf[MAX];
 
     if(argc==1){
         printf("use -a to add alias\n");
         printf("use -d to delete alias\n");
+        printf("use -r to read alias file\n");
+    }
+    else if(argc ==2){
+        if(strcmp[argv[1]=="-r"){
+            fd = open(".alias",O_RDONLY);
+            while((n=read(fd,buf,MAX))>0){
+                printf("%s",buf);
+            }
+            if(n<0){
+                perror(".alias read error");
+                exit(1)
+            }
+            return 0;
+        }
+        else{
+            printf("error\n");        
+            exit(1);
+        }
     }
     else if(argc ==3){
         if(strcmp(argv[1],"-a")==0)
             option=1;
         else if(strcmp(argv[1],"-d")==0)
-            option=0;
+            option=0;        
         else{
             printf("Unknown option\n");
             exit(1);
